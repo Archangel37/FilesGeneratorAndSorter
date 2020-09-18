7e499e79b694fe8d4e3fd4ec396a61d62a4950fc
Arcangel37 <djarchangel3737@gmail.com>
2020-09-18 11:16:58 +0300

    added tests for Extensions.Compare

--- FileSorter/Tests/OtherTests.cs
+++ FileSorter/Tests/OtherTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Diagnostics.CodeAnalysis;
 using FileSorter.Implementations;
 using FileSorter.Models;
@@ -9,6 +8,18 @@ namespace Tests
     [ExcludeFromCodeCoverage]
     public class OtherTests
     {
+        private static readonly string[] ArrayForCompare =
+        {
+            "A",
+            "A ",
+            "AB",
+            "Z",
+            "ZZ",
+            "a",
+            "a ",
+            "ab"
+        };
+        
         [Fact]
         public void Test_Digits_IfChain()
         {
@@ -20,7 +31,6 @@ namespace Tests
             }
         }
         
-        
         [Fact]
         public void Test_ValidateConfig()
         {
@@ -31,6 +41,18 @@ namespace Tests
             Assert.True(cfg.WriteMemoryBufferBytes == 524260);
             Assert.True(cfg.OutputFileName == "result.txt");
             Assert.True(cfg.TemporaryFilesExtension == "");
+        }
+        
+        [Fact]
+        public void Test_CompareExtension()
+        {
+            for (var i = 0; i < ArrayForCompare.Length - 1; i++)
+                Assert.True(ArrayForCompare[i].Compare(ArrayForCompare[i + 1]) == -1);
+            
+            for (var j = ArrayForCompare.Length - 1; j > 1; j--)
+                Assert.True(ArrayForCompare[j].Compare(ArrayForCompare[j - 1]) == 1);
+            
+            Assert.True("ABC".Compare("ABC") == 0);
         }
     }
 }
